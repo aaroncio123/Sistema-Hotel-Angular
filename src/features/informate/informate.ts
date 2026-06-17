572968b4160828be658dcc54c3d2e4ff435f1625
Aaron <aaronhorna03@gmail.com>
2026-06-17 08:01:08 -0500

    Se testea la creación de categorias y todo genial

--- src/features/informate/informate.ts
+++ src/features/informate/informate.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Categoria } from '../../models/categoria.model';
+import { CategoriaService } from '../../service/categoria.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-informate',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './informate.html',
   styleUrl: './informate.css',
 })
-export class Informate {
+export class Informate implements OnInit {
   categorias: Categoria[] = [];
+  private categoriaService = inject(CategoriaService);
+
+  ngOnInit() {
+    this.cargarCategorias();
+  }
+
+  cargarCategorias() {
+    this.categoriaService.listar().subscribe({
+      next: (data) => this.categorias = data,
+      error: (err) => console.error('Error al cargar categorías', err)
+    });
+  }
 }
